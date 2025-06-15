8cd29cf1a309f307c763e903cb50baa28f7191f4
BeamingMonkey <arunraj3388@gmail.com>
2025-06-15 07:46:03 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
-import os
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+application = Flask(__name__)
+
+@application.route('/')
 def hello():
     return "Live from GitHub CI/CD on Elastic Beanstalk 💥"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # EB sets this env variable
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=8000)
